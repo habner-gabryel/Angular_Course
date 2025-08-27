369ae930df6033f59907a8f72bbe82b1de04b1b3
habner-gabryel <habner.gabryel.cc@gmail.com>
2025-08-27 20:59:03 -0300

    Alterações feitas na aula 3

--- portfolio-landing/src/app/app.routes.ts
+++ portfolio-landing/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Header } from './component/header/header';
 import { PaginaCadastro } from './pages/pagina-cadastro/pagina-cadastro';
 import { PaginaContagem } from './pages/pagina-contagem/pagina-contagem';
+import { PaginaListagem } from './pages/pagina-listagem/pagina-listagem';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: 'contagem',
         component: PaginaContagem,        
+    },
+    {
+        path: 'usuarios',
+        component: PaginaListagem,
     }
 ];
--- portfolio-landing/src/app/pages/pagina-contagem/pagina-contagem.ts
+++ portfolio-landing/src/app/pages/pagina-contagem/pagina-contagem.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 import { Contador } from "../../component/contador/contador";
+import { ContadorService } from '../../services/contador-service';
+import { Contador1 } from "../../component/contador-1/contador-1";
+import { Contador2 } from "../../component/contador-2/contador-2";
 
 @Component({
   selector: 'app-pagina-contagem',
-  imports: [Contador],
+  imports: [Contador1, Contador2],
   templateUrl: './pagina-contagem.html',
   styleUrl: './pagina-contagem.scss'
 })
 export class PaginaContagem {
-  valor: number = 0;
+
+  constructor(private contadorService: ContadorService) {}
 
 
-  incrementar(): void {
-    this.valor++;
-  }
-
-  decrementar(): void {
-    this.valor--;
+  apresentarValor(): number {
+    return this.contadorService.getValor();
   }
 
 }
